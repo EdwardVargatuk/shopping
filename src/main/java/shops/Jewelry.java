72321b082ed9f387831ce30c01fb026f5be25f36
EdwardVargatuk <44446341+EdwardVargatuk@users.noreply.github.com>
2018-11-05 16:15:12 +0200

    add some1

--- src/main/java/shops/Jewelry.java
+++ src/main/java/shops/Jewelry.java
@@ -1,4 +1,20 @@
 package shops;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Jewelry extends Shop {
+
+    private List<Item> jewelryItemsList;
+    private String[] names = {"Ring", "Necklaces", "Bracer"};
+    private double[] prices = {999,9, 4999,9, 3000};
+
+    public Jewelry() {
+        this.jewelryItemsList = new ArrayList<Item>(createItemsList(names, prices));
+    }
+
+    public List<Item> getJewelryItemsList() {
+        return jewelryItemsList;
+    }
+
 }
